68b7622d2e2e61e32f82e5bc94ef7b8d9f1219a8
Nastya Gordeeva <nastyagordeeva72@gmail.com>
2023-04-24 11:01:27 +0300

    Initial set up of the project

---
+++ src/main/java/Main.java
@@ -0,0 +1,15 @@
+import java.io.File;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        File file = new File("src/main/resources/studentsUnique.csv");
+        ReaderCSV readerCSV = new ReaderCSV();
+
+        // READ STUDENTS FROM FILE INTO LIST
+        List<Student> students = readerCSV.readFromFile(file);
+
+        // SHOW THEM ON THE SCREEN
+        students.stream().forEach(System.out::println);
+    }
+}
